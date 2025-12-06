62127a8b72e1c11c680a001624fa28f4a0c0a275
muntaha-fatima <shazsabir6@gmail.com>
2025-12-07 00:02:20 +0500

    book  pages

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://muntaha-fatima.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/robotic-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'muntaha-fatima', // Usually your GitHub org/user name.
+  projectName: 'robotic-book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
